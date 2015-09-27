590ba0f8becb7c07207d9388f8d124f962a1b2c7
TL <janneisyvidals@gmail.com>
2015-09-27 17:46:14 -0400

    add array of quotes

--- app/src/main/java/abassawo/c4q/nyc/ecquo/Activities/LauncherActivity.java
+++ app/src/main/java/abassawo/c4q/nyc/ecquo/Activities/LauncherActivity.java
@@ -1,6 +1,7 @@
 package abassawo.c4q.nyc.ecquo.Activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -17,25 +18,28 @@ public class LauncherActivity extends Activity {
     private static final int STOPSPLASH = 0;
     private Handler mSplashHandler;
     public TextView showQuote;
+    Context context;
 
     // Handler start
     {
-        mSplashHandler = new Handler()
-        {
+        mSplashHandler = new Handler() {
             @Override
-            public void handleMessage(Message msg)
-            {
+            public void handleMessage(Message msg) {
                 super.handleMessage(msg);
-                switch (msg.what)
-                {
+                switch (msg.what) {
                     case STOPSPLASH:
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        Intent intent = new Intent(LauncherActivity.this, MainActivity.class);
                         startActivity(intent);
                         finish();
                 }
             }
         };
-    }// Handler end
+    }
+// Handler end
+
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
 
 
     @Override
@@ -43,19 +47,26 @@ public class LauncherActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
         //Get array of quotes
-
-
         showQuote = (TextView) findViewById(R.id.quote_text);
         String[] quotes = getResources().getStringArray(R.array.quotes);
         int randomIndex = new Random().nextInt(quotes.length);
         String quote = quotes[randomIndex];
         showQuote.setText(quote);
 
+//        {
+//            new Handler().postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                /* Create an Intent that will start the Menu-Activity. */
+//                    Intent mainIntent = new Intent(LauncherActivity.this, MainActivity.class);
+//                    LauncherActivity.this.startActivity(mainIntent);
+//                    LauncherActivity.this.finish();
+//                }
+//            }, SPLASHTIME);
 
 
 
-        // to start the handler
-        mSplashHandler.sendEmptyMessageDelayed(STOPSPLASH, SPLASHTIME);
+            // to start the handler
+         mSplashHandler.sendEmptyMessageDelayed(STOPSPLASH, SPLASHTIME);
+        }
     }
-
-}
