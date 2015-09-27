9e3472fdb6346dd7c0a85f2f2f9845d0430d595a
TL <janneisyvidals@gmail.com>
2015-09-27 10:42:19 -0400

    Splash

--- app/src/main/java/abassawo/c4q/nyc/ecquo/Activities/LauncherActivity.java
+++ app/src/main/java/abassawo/c4q/nyc/ecquo/Activities/LauncherActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.widget.TextView;
+
+import java.util.Random;
 
 import abassawo.c4q.nyc.ecquo.R;
 
@@ -13,6 +16,7 @@ public class LauncherActivity extends Activity {
     private static final int SPLASHTIME = 3000;
     private static final int STOPSPLASH = 0;
     private Handler mSplashHandler;
+    public TextView showQuote;
 
     // Handler start
     {
@@ -33,10 +37,22 @@ public class LauncherActivity extends Activity {
         };
     }// Handler end
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        //Get array of quotes
+
+
+        showQuote = (TextView) findViewById(R.id.quote_text);
+        String[] quotes = getResources().getStringArray(R.array.quotes);
+        int randomIndex = new Random().nextInt(quotes.length);
+        String quote = quotes[randomIndex];
+        showQuote.setText(quote);
+
+
+
 
         // to start the handler
         mSplashHandler.sendEmptyMessageDelayed(STOPSPLASH, SPLASHTIME);
